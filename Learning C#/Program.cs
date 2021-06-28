7e760b7e52e50d51cb4041922e80920dda3b8ef3
nate hare (spekky) <84371457+RasaNovum@users.noreply.github.com>
2021-06-28 01:51:49 -0700

    Learning C# Setup Commit

---
+++ Learning C#/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Learning_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ListsAndArraysPractice(); --- Call Lists Practice Method
+        }
+
+        static void ListsAndArraysPractice() {
+
+            string[] movies = {"Lord of the Rings", "Harry Potter", "Star Trek", "The F Word", "Seven Psycopaths"};
+
+            for (int i = 0; i < movies.Length; i++)
+            {
+                Console.WriteLine(movies[i]);
+            }
+
+            // Wait for keypress before closing
+            Console.ReadKey();
+
+        }
+    }
+}
