065fa1adf8e19e5abb60dc7c394173bd70bad0b2
nate hare (spekky) <84371457+RasaNovum@users.noreply.github.com>
2021-06-28 02:37:16 -0700

    Extra While Loop practice

--- Learning C#/Program.cs
+++ Learning C#/Program.cs
@@ -7,7 +7,7 @@ namespace Learning_C_
         static void Main(string[] args)
         {
             /*
-            --- Lists and Arrays practice call ---
+            --- Lists and Arrays practice ---
 
             ListsAndArraysPractice();
             */
@@ -19,7 +19,15 @@ namespace Learning_C_
             Console.WriteLine(squaredNum);
             */
 
+            /*
+            --- While Loop Practice ---
             DiceRoll();
+            */
+
+            /*
+            --- Extra while loop practice ---
+            */
+            DoubleDiceRoll();
 
             // Keep terminal window open until keypress
             Console.ReadKey();
@@ -63,6 +71,31 @@ namespace Learning_C_
             }
 
             Console.WriteLine($"It took {attemptsNum} attempts to roll a 6.");
+        }
+
+        static void DoubleDiceRoll() {
+
+            Console.WriteLine("Press Enter to roll the dice:");
+            int D1 = 0;
+            int D2 = 1;
+            int attempts = 0;
+
+            while (D1 != D2) {
+                Console.ReadKey();
+
+                Random numD1 = new Random();
+                Random numD2 = new Random();
+
+                D1 = numD1.Next(1,7);
+                D2 = numD2.Next(1,7);
+
+                Console.WriteLine($"You Rolled a {D1} and a {D2}.");
+                attempts++;
+
+            }
+
+            Console.WriteLine($"\nYou Rolled a pair of {D1}'s!");
+            Console.WriteLine($"It took {attempts} attempts to roll that pair.");
         }
     }
 }
